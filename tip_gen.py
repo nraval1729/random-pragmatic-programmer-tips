a8a52f1fd2a8242860b623c88895dd96b1477997
Nisarg Rawal <nisarg_rawal@brown.edu>
2016-06-19 21:08:20 -0400

    Error checking on tip_gen.py

--- tip_gen.py
+++ tip_gen.py
@@ -3,6 +3,10 @@ import random
 import sys
 
 def main():
+	if len(sys.argv) != 2:
+		print "Error: Input the /path/to/pickled/file"
+		sys.exit()
+
 	with open(sys.argv[1], "rb") as p:
 		tip_to_description = pickle.load(p)
 
